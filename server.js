f3f1ef72ea965b8a6009e766940f1dc0657d9cb3
miki <miki.wiik@gmail.com>
2012-06-10 22:17:09 +0300

    First commit

---
+++ server.js
@@ -0,0 +1,40 @@
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs');
+
+// NOTE: Ubuntu does not allow 80 for standard users
+app.listen(8080);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  /*socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });*/
+
+	// TODO: Act as a message broker
+    socket.on('chat', function (data) {
+        console.log("chat|" + socket.id +":" + JSON.stringify(data));
+
+        //console.log(JSON.stringify(io.sockets.sockets));
+
+        data.id = socket.id;
+        // TODO: Add messageIs add sender id.
+
+        socket.broadcast.emit('update', data);
+
+        // Send to specific <namespace>.socket(<id>).send()
+	});
+});
