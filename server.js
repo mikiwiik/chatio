935817580263d236f9d2a4f3b12a927992e65222
Miki Wiik <miki.wiik@service-flow.com>
2012-06-15 14:51:36 +0300

    Whitespace for legibility

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ var app = require('http').createServer(handler)
 app.listen(8888);
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+    fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+            if (err) {
+              res.writeHead(500);
+              return res.end('Error loading index.html');
+            }
 
-    res.writeHead(200);
-    res.end(data);
-  });
+            res.writeHead(200);
+            res.end(data);
+    });
 }
 
 io.sockets.on('connection', function (socket) {
